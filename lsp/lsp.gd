95a7e0351cca08cae5bb50afd5420c70efd13ce5
Phillip Wood <Phillipwood2023@outlook.com>
2025-05-10 22:36:09 -0400

    removed lsp from project files, user will be required to download the lsp. Going to add a setting for lsp install path. Starting and connecting to lsp via gdscript.

---
+++ lsp/lsp.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var client := StreamPeerTCP.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Start the LSP
+	#TODO: change this to a path configured by the application...
+	var pid = OS.execute("D:\\dev\\tooling\\lua-lsp\\bin\\lua-language-server.exe", ["--socket=5050"], false)
+	if pid == -1:
+		print("LSP failed to Execute")
+	else:
+		print("LSP running pid ", pid)
+		connect_to_lsp("127.0.0.1", 5050)
+
+func connect_to_lsp(ip: String, port: int):
+	var err = client.connect_to_host(ip, port)
+	if err == OK:
+		print("Connected to Lua LSP Server!")
+	else:
+		print("Failed to connect:", err)
+
+func send_initialize_request():
+	var request = {
+		"jsonrpc": "2.0",
+		"id": 1,
+		"method": "initialize",
+		"params": {
+			"processId": 1234,
+			"rootUri": "file:///path/to/project",
+			"capabilities": {}
+		}
+	}
+	send_json(request)
+	
+func send_json(data):
+	var json_string = JSON.stringify(data)
+	client.put_data((json_string + "\n").to_utf8_buffer())
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if client.get_available_bytes() > 0:
+		var response = client.get_string(client.get_available_bytes())
+		print("LSP Respone:", response)
